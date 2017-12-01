e08b6d238bf2e71408a99b4d4912c8f9aa21222d
Sébastien HOUZÉ <cto@verylastroom.com>
2017-12-01 11:33:11 +0100

    :new: [Healthcheck] Add ChainHealthcheck
    Fixes #8

---
+++ src/Component/Healthcheck/ChainHealthcheck.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Ubirak package.
+ *
+ * (c) Ubirak team <team@ubirak.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Ubirak\Component\Healthcheck;
+
+final class ChainHealthcheck implements Healthcheck
+{
+    private $healthchecks;
+
+    public function __construct(array $healthchecks = [])
+    {
+        array_map([$this, 'add'], $healthchecks);
+    }
+
+    public function isReachable(Destination $destination): bool
+    {
+        foreach ($this->healthchecks as $healthcheck) {
+            if (false === $healthcheck->isReachable($destination)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function add(Healthcheck $healthcheck)
+    {
+        $this->healthchecks[] = $healthcheck;
+    }
+}
---
+++ src/Component/Healthcheck/tests/Units/ChainHealthcheck.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Ubirak package.
+ *
+ * (c) Ubirak team <team@ubirak.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Ubirak\Component\Healthcheck\Tests\Units;
+
+use atoum;
+use Ubirak\Component\Healthcheck\Destination;
+
+class ChainHealthcheck extends atoum
+{
+    public function test first failure should stop chain()
+    {
+        $this
+            ->given(
+                $healthcheck1 = $this->mockHealthcheck(false),
+                $healthcheck2 = $this->mockHealthcheck(true),
+                $this->newTestedInstance([$healthcheck1, $healthcheck2])
+            )
+            ->when(
+                $result = $this->testedInstance->isReachable(new Destination('http://localhost'))
+            )
+            ->then
+                ->boolean($result)->isFalse()
+                ->mock($healthcheck1)->call('isReachable')->once()
+                ->mock($healthcheck2)->call('isReachable')->never()
+        ;
+    }
+
+    public function test chain is not stopped if all are reachable()
+    {
+        $this
+            ->given(
+                $healthcheck1 = $this->mockHealthcheck(true),
+                $healthcheck2 = $this->mockHealthcheck(true),
+                $this->newTestedInstance([$healthcheck1, $healthcheck2])
+            )
+            ->when(
+                $result = $this->testedInstance->isReachable(new Destination('http://localhost'))
+            )
+            ->then
+                ->boolean($result)->isTrue()
+                ->mock($healthcheck1)->call('isReachable')->once()
+                ->mock($healthcheck2)->call('isReachable')->once()
+        ;
+    }
+
+    private function mockHealthcheck(bool $isReachable)
+    {
+        $mock = new \mock\Ubirak\Component\Healthcheck\Healthcheck();
+        $this->calling($mock)->isReachable = $isReachable;
+
+        return $mock;
+    }
+}
